4775db99ccad83eb2b8a287515b75315a41b3d8e
saurav kumar <sauravkumar91937@gmail.com>
2025-06-01 02:11:13 +0530

    update

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -26,8 +26,14 @@ authRouter.post("/signup", async (req, res) => {
       password: passwordHash,
     });
 
-    await user.save();
-    res.send("user added sucessfully!");
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.json({ message: "user added sucessfully!", data: savedUser });
   } catch (err) {
     res.status(400).send("Error Saving the user:" + err.message);
   }
