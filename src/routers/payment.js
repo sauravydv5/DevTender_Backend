226789618656435f820761fa066f93ea9c7fe17d
saurav kumar <sauravkumar91937@gmail.com>
2025-06-16 01:27:31 +0530

    update payment

--- src/routers/payment.js
+++ src/routers/payment.js
@@ -4,25 +4,40 @@ const userAuth = require("../middleware/auth");
 const paymentRouter = express.Router();
 const instance = require("../utils/razorpay");
 const Razorpay = require("razorpay");
+const Payment = require("../models/payment");
 
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
+    const { membershipType } = req.body;
+    const { firstName, lastName, emailId } = req.user;
     const order = await instance.orders.create({
       amount: 2000, // in paisa
       currency: "INR",
       receipt: "receipt#1",
       notes: {
-        firstName: "value3",
-        lastName: "value2",
-        membershipType: "silver",
+        firstName,
+        lastName,
+        emailId,
+        membershipType: membershipType,
       },
     });
 
     console.log(order);
 
-    res.status(201).json({
-      message: "Payment created successfully",
-      order,
+    const payment = new Payment({
+      userId: req.user._id,
+      orderId: order.id,
+      amount: order.amount,
+      status: order.status,
+      currency: order.currency,
+      receipt: order.receipt,
+      notes: order.notes,
+    });
+
+    const savedPayment = await payment.save();
+
+    res.json({
+      ...savedPayment.toJSON(), // sending the payment details to the client
     });
   } catch (error) {
     console.error("Razorpay error:", error);
